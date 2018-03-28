a600b79ec3fb7ef1e6bf1dd2b5bc9c329d9c2b14
davisilva4222 <davisilva4222@gmail.com>
2018-03-27 21:24:51 -0300

    fetch users github

--- app/main/App.jsx
+++ app/main/App.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import axios from 'axios'
 
-import HelloWorld from '../components/HelloWorld'
+import UserList from '../components/UserList'
 
-const App = () => {
-    return (
-        <HelloWorld text='Olá' />
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            list: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://api.github.com/users')
+            .then(response => {
+                this.setState({
+                    list: response.data
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>GITHUB API GET</h1>
+                <UserList list={this.state.list} />
+            </div>
+        )
+    }
 }
 
 export default App
